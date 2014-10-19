9a0a8d495ff84dc63126079031d47239fd6a3389
beetleman <nigrantis.tigris@gmial.com>
2014-10-19 22:52:51 +0200

    Dodanie schematu logowania i wylogowywania.

--- zabiniec/routes.py
+++ zabiniec/routes.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from .views import (
     index,
+    login,
+    lists,
+    logout,
 )
 
 
 def create_routes(app):
     app.add_url_rule('/', 'index', index)
+    app.add_url_rule('/login', 'login', login,  methods=['GET', 'POST'])
+    app.add_url_rule('/logout', 'logout', logout)
+    app.add_url_rule('/lists', 'lists', lists)
+
--- zabiniec/views.py
+++ zabiniec/views.py
@@ -1,9 +1,51 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flask.ext.login import login_user, login_required, logout_user
 
 from .utils import porn
+from .models import User
 
 
 @porn
 def index():
-    return render_template('index.html', title='index')
+    return render_template('index.html', title='Ktoś?')
+
+
+@porn
+def login():
+    username = request.args.get('kto')
+    is_failed = False
+    try:
+        user = User.get(User.username == username)
+    except User.DoesNotExist:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        answer = request.form.get('answer')
+        if answer.lower() == user.answer.lower():
+            login_user(user)
+            return redirect(url_for('lists'))
+        else:
+            is_failed = True
+    return render_template(
+        'login.html',
+        title='Że jak?',
+        question=user.question,
+        username=username,
+        is_failed=is_failed
+    )
+
+
+@login_required
+@porn
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@login_required
+@porn
+def lists():
+    return render_template(
+        'lists.html',
+        title='Planista-Żabista!',
+    )
