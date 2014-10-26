4ffb304953f4c798baf148d2e3c192f3389b9351
beetleman <nigrantis.tigris@gmial.com>
2014-10-26 14:21:37 +0100

    Dodaje możliwość zmiany pytania (mechanizm logowania).

--- zabiniec/routes.py
+++ zabiniec/routes.py
@@ -4,6 +4,7 @@ from .views import (
     login,
     lists,
     logout,
+    change_passwd,
 )
 
 
@@ -12,4 +13,9 @@ def create_routes(app):
     app.add_url_rule('/login', 'login', login,  methods=['GET', 'POST'])
     app.add_url_rule('/logout', 'logout', logout)
     app.add_url_rule('/lists', 'lists', lists)
-
+    app.add_url_rule(
+        '/change_passwd',
+        'change_passwd',
+        change_passwd,
+        methods=['GET', 'POST']
+    )
--- zabiniec/views.py
+++ zabiniec/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, request, redirect, url_for
-from flask.ext.login import login_user, login_required, logout_user
+from flask.ext.login import login_user, login_required, logout_user, current_user
 
 from .utils import porn
 from .models import User
@@ -48,4 +48,28 @@ def lists():
     return render_template(
         'lists.html',
         title='Planista-Żabista!',
+    )
+
+
+@login_required
+@porn
+def change_passwd():
+    is_failed = False
+    complementary_user = current_user.get_complementary()
+    if request.method == 'POST':
+        answer = request.form.get('answer', None)
+        question = request.form.get('question', None)
+        if len(question) < 10 and len(answer) < 5:
+            is_failed = True
+        else:
+            complementary_user.answer = answer
+            complementary_user.question = question
+            complementary_user.save()
+
+    return render_template(
+        'change_passwd.html',
+        title='Hasła zmienianie, śąwiata zaskakiwanie',
+        is_failed=is_failed,
+        answer=complementary_user.answer,
+        question=complementary_user.question
     )
